5998b3cfc06b98bc08e7fedfb872cec6e6954870
matuu <matu.varela@gmail.com>
2016-02-08 10:15:28 -0300

    mas de supervisor

--- controla/controla/celeryapp.py
+++ controla/controla/celeryapp.py
@@ -5,7 +5,10 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'controla.settings.local')
+if settings.DEBUG:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'controla.settings.local')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'controla.settings.production')
 
 app = Celery('controla')
 
