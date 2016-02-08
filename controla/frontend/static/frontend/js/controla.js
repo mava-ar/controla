091d37314ff441617f634f478fdaf7b0384c4d34
matuu <matu.varela@gmail.com>
2016-02-08 08:55:26 -0300

    vuelvo a definir globalmente a showConfirm

--- controla/frontend/static/frontend/js/controla.js
+++ controla/frontend/static/frontend/js/controla.js
@@ -1,35 +1,34 @@
 /**
  * Created by matuu on 29/01/16.
  */
-(function($) {
-    function showConfirm(title, message, callback_yes, callback_no) {
-        BootstrapDialog.confirm({
-            title: title,
-            message: message,
-            type: BootstrapDialog.TYPE_DANGER,
-            closable: true, // <-- Default value is false
-            draggable: true, // <-- Default value is false
-            btnCancelLabel: 'No, cancelar!', // <-- Default value is 'Cancel',
-            btnOKLabel: 'Si, continuar!', // <-- Default value is 'OK',
-            btnOKClass: 'btn-primary',
-            callback: function (result) {
-                // result will be true if button was click, while it will be false if users close the dialog directly.
-                if (result) {
-                    try {
-                        callback_yes();
-                    } catch (e) {
-                        console.log(e);
-                    }
-                } else {
-                    try {
-                        if (callback_no != undefined)
-                            callback_no();
-                    } catch (e) {
-                        console.log(e);
-                    }
 
+showConfirm = function(title, message, callback_yes, callback_no) {
+    BootstrapDialog.confirm({
+        title: title,
+        message: message,
+        type: BootstrapDialog.TYPE_DANGER,
+        closable: true, // <-- Default value is false
+        draggable: true, // <-- Default value is false
+        btnCancelLabel: 'No, cancelar!', // <-- Default value is 'Cancel',
+        btnOKLabel: 'Si, continuar!', // <-- Default value is 'OK',
+        btnOKClass: 'btn-primary',
+        callback: function (result) {
+            // result will be true if button was click, while it will be false if users close the dialog directly.
+            if (result) {
+                try {
+                    callback_yes();
+                } catch (e) {
+                    console.log(e);
                 }
+            } else {
+                try {
+                    if (callback_no != undefined)
+                        callback_no();
+                } catch (e) {
+                    console.log(e);
+                }
+
             }
-        });
-    }
-})(jQuery);
+        }
+    });
+}
